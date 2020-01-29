26668f250a1ee008065b2876b55320e6ef8458a4
Alihan Namli <namlialihan@gmail.com>
2020-01-29 01:37:06 -0500

    test with params

--- src/test/java/restapitests/SpartanTestsWithPath.java
+++ src/test/java/restapitests/SpartanTestsWithPath.java
@@ -6,6 +6,9 @@ import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import utils.ConfigurationReader;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
@@ -54,6 +57,59 @@ public class SpartanTestsWithPath {
         assertEquals("Female", gender);
         assertEquals(3312820936L, phone);
 
+        assertEquals(200, response.statusCode());
     }
+
+    @Test
+    public void getAllSpartans_using_path_with_list(){
+        Response response = get("/spartans/");
+
+        //extract first id
+        int firstId = response.path("id[0]");
+        System.out.println("firstID: " + firstId);
+
+        //extract first name
+        String first1stName = response.path("name[0]");
+        System.out.println("first1stName = " + first1stName);
+
+        //get last firstname
+        String lastFirstName = response.path("name[-1]");
+        System.out.println("lastFirstName = " + lastFirstName);
+
+        //extract all firstNames and print out
+        List<String> names = response.path("name");
+        System.out.println("number of names "+names.size());
+        System.out.println("names = " + names);
+
+        //extract all phone numbers
+        List<Object> phoneNumbers = response.path("phone");
+        for (Object ph : phoneNumbers){
+            System.out.println("ph = " + ph);
+        }
+    }
+        @Test
+        public void getCountries_And_Extract_nested_key_values_using_path(){
+        RestAssured.baseURI = ConfigurationReader.getProperty("hrapi.baseuri");
+    //    Response response = get("/countries?q={\"region_id\":2}");
+        Response response = given().queryParam("q","{\"region_id\":2}")
+                .when().get("/countries");
+
+        String firstCountryID = response.path("items.country_name[0]");
+        String firstCountryName = response.path("items.country_name[0]");
+
+            System.out.println("firstCountryID = "+firstCountryID);
+            System.out.println("firstCountryName = " + firstCountryName);
+
+            // get all country names and print them out
+            List<String> countries = response.path("items.country_name");
+            System.out.println("countries = " + countries);
+
+            //asssert that all region ids equal to 2
+            List<Integer> regionIDs = response.path("items.region_id");
+            for (int ri : regionIDs) {
+            assertEquals(2, ri);
+            }
+        }
+
 
 }
